82bc005218cafc03e867c885c56a91be04a59d60
Saemkenya <187ca@gmail.com>
2020-05-26 07:10:00 +0300

    Milestone tracking on readme

--- js/main.js
+++ js/main.js
@@ -37,6 +37,8 @@ $(function () {
 
   var animate = function () {
     cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
   };
